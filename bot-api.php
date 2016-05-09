66a0f8245d2b2cc44f9db922efa4e2708618a893
Timur <injener89@gmail.com>
2016-05-09 18:38:00 +0500

    test19

--- bot-api.php
+++ bot-api.php
@@ -19,10 +19,24 @@ function send_post($url,$data){
     curl_close ($ch);
     return $data;
 }
+function send_get($url,$data = 0){
+    $ch=curl_init();
+   // curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, 0); // отключение сертификата
+   // curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, FALSE); // отключение сертификата
+    curl_setopt ($ch, CURLOPT_URL, $url );
+    curl_setopt ($ch, CURLOPT_HTTPHEADER,array('Content-Type: text/xml'));
+    curl_setopt ($ch, CURLOPT_RETURNTRANSFER,1);
+    curl_setopt ($ch, CURLOPT_POST, 1);
+    curl_setopt ($ch, CURLOPT_POSTFIELDS, $data );
+    curl_setopt ($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)");
+    $data=curl_exec ($ch);
+    curl_close ($ch);
+    return $data;
+}
 function sendMessage($access_token,$param) {
   $api = 'https://api.telegram.org/bot' . $access_token;  
   //return file_get_contents($api . '/'.$param);
-  return send_post($api.'/'.$param->method,$param->param);
+  return send_get($api.'/'.$param->method.'?'.$param->param);
 } 
 
 
