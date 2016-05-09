f2435873153b0b56ebbf6e36df35b7e090db34b5
Timur <injener89@gmail.com>
2016-05-09 17:44:27 +0500

    test16

--- bot-api.php
+++ bot-api.php
@@ -7,7 +7,7 @@
  */
 function sendMessage($access_token,$param) {
   $api = 'https://api.telegram.org/bot' . $access_token;  
-  file_get_contents($api . '/'.$param);
+  return file_get_contents($api . '/'.$param);
 } 
 function send_post($url,$data){
     $ch=curl_init();
@@ -34,97 +34,11 @@ if(isset($_GET['respons']) && $_GET['respons'] != "")
     $res = json_decode($result);
     if(isset($output['message']['chat']['id'])){
         $param = send_post($res->url,$telegram);
-        sendMessage($res->bot_token,$param);
+        $telegramResult = sendMessage($res->bot_token,$param);
+        if($res->return_telegram_is == 1){
+            send_post($res->return_telegram,$telegramResult);
+        }
     }   
 }
-
+echo "Bad request!";
 exit;
-$rrr = file_get_contents('php://input');
-$output = json_decode(file_get_contents('php://input'), TRUE);
-if(isset($output['message']['chat']['id'])){
-   
-    
-    $ww = send_post('http://www.tt.uz/bot_file.php',$rrr);
-    sendMessage($ww);
-} 
-
-$output = json_decode(file_get_contents('php://input'), TRUE);
-$chat_id = $output['message']['chat']['id'];
-$first_name = $output['message']['chat']['first_name'];
-$message = $output['message']['text'];
-
-
-$emoji = array(
-  'preload' => json_decode('"\uD83D\uDE03"'), // Улыбочка.
-  'weather' => array(
-    'clear' => json_decode('"\u2600"'), // Солнце.
-    'clouds' => json_decode('"\u2601"'), // Облака.
-    'rain' => json_decode('"\u2614"'), // Дождь.
-    'snow' => json_decode('"\u2744"'), // Снег.
-  ),
-);
-
-/**
- * Получаем команды от пользователя.
- */  
-switch($message) {
-  // API погоды предоставлено OpenWeatherMap.
-  // @see http://openweathermap.org
-  case '/pogoda':
-    // Отправляем приветственный текст.
-    $preload_text = 'Одну секунду, ' . $first_name . ' ' . $emoji['preload'] . ' Я уточняю для вас погоду..';
-    sendMessage($chat_id, $preload_text);
-    // API key для OpenWeatherMap.
-    // Формирование ответа.
-    $weather_text = 'Сейчас в Ташкенте. Температура воздуха: 31°C. Ветер  10м/сек.';
-    // Отправка ответа пользователю Telegram.
-    sendMessage($chat_id, $weather_text);
-    break;
-  default:
-    break;
-}  
-/*<?php
-
-$access_token = '187767986:AAFLK_h8SMMG1mH8cDS64xfE5Lb6LRyo2vU';
-$api = 'https://api.telegram.org/bot' . $access_token;
-
-$output = json_decode(file_get_contents('php://input'), TRUE);
-$chat_id = $output['message']['chat']['id'];
-$first_name = $output['message']['chat']['first_name'];
-$message = $output['message']['text'];
-
-$emoji = array(
-  'preload' => json_decode('"\uD83D\uDE03"'), // Улыбочка.
-  'weather' => array(
-    'clear' => json_decode('"\u2600"'), // Солнце.
-    'clouds' => json_decode('"\u2601"'), // Облака.
-    'rain' => json_decode('"\u2614"'), // Дождь.
-    'snow' => json_decode('"\u2744"'), // Снег.
-  ),
-);
-  
-switch($message) {
-  // API погоды предоставлено OpenWeatherMap.
-  // @see http://openweathermap.org
-  case '/pogoda':
-    // Отправляем приветственный текст.
-    $preload_text = 'Одну секунду, ' . $first_name . ' ' . $emoji['preload'] . ' Я уточняю для вас погоду..';
-    sendMessage($chat_id, $preload_text);
-    // API key для OpenWeatherMap.
-    // Формирование ответа.
-    $weather_text = 'Сейчас в Ташкенте. Температура воздуха: 31°C. Ветер  10м/сек.';
-    // Отправка ответа пользователю Telegram.
-    sendMessage($chat_id, $weather_text);
-    break;
-    case 'Ne':
-    $weather_text = 'Атан басы козявка';
-    // Отправка ответа пользователю Telegram.
-    sendMessage($chat_id, $weather_text);
-    break;
-  default:
-    break;
-}  
-
-function sendMessage($chat_id, $message) {
-  file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message));
-}
